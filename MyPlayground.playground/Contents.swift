c883c139391d86a84e2a1dbdf0f314456df1c80c
harapeko <gmail@harapeko.dev>
2019-11-27 00:17:11 +0900

    add playground

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,41 @@
+// 加算
+1 + 2 + 3
+// 文字列
+"こんちは"
+// 文字列 連結
+"Hello, " + "playground"
+// デバッグエリアに表示される
+print("hogehoge")
+// 変数
+var hoge = "hoge"
+var fuga: String = "fugafuga"
+var num: Int = 3
+// 定数
+let value: String = "定数"
+// value = "定数2" おこられる
+// キャスト
+let a = String(1)
+Int("1")
+// 変数展開
+"りんごを\(a)個"
+Int.max
+Int.min
+Int8.max
+Int8.min
+Int16.max
+Int16.min
+Int32.max
+Int32.min
+Int64.max
+Int64.min
+
+UInt.max
+UInt.min
+UInt8.max
+UInt8.min
+UInt16.max
+UInt16.min
+UInt32.max
+UInt32.min
+UInt64.max
+UInt64.min
