c4b095f6fbbafce21f574ddec99b423a7eef33cf
harapeko <gmail@harapeko.dev>
2019-12-08 17:44:38 +0900

    add 削除処理

--- MyTodoList/MyTodoList/ViewController.swift
+++ MyTodoList/MyTodoList/ViewController.swift
@@ -128,6 +128,29 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
             // エラー処理なし
         }
     }
+
+    // セルを削除したときの処理
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        // 削除処理かどうか
+        if editingStyle == UITableViewCell.EditingStyle.delete {
+            // TODOリストから削除
+            todoList.remove(at: indexPath.row)
+
+            // セル削除
+            tableView.deleteRows(at: [indexPath], with: UITableView.RowAnimation.fade)
+
+            // データ保存。Data型にシリアライズする
+            do {
+                let data: Data = try NSKeyedArchiver.archivedData(withRootObject: todoList, requiringSecureCoding: true)
+                // UserDefaultsに保存
+                let userDefaults = UserDefaults.standard
+                userDefaults.set(data, forKey: "todoList")
+                userDefaults.synchronize()
+            } catch {
+                // エラー処理なし
+            }
+        }
+    }
 }
 
 // 独自クラスをシリアライズする際には、NSOBjectを継承し
