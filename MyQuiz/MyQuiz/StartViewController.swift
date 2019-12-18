967619568bd082f5206e5688310fe184017bd718
harapeko <gmail@harapeko.dev>
2019-12-19 03:04:28 +0900

    クイズアプリ開始画面の実装した

--- MyQuiz/MyQuiz/StartViewController.swift
+++ MyQuiz/MyQuiz/StartViewController.swift
@@ -6,4 +6,37 @@
 //  Copyright © 2019 dev.harapeko. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class StartViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // 次の画面に繊維される前の準備処理
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // super.prepare(for: segue, sender: sender)
+        // 問題文を読み込む
+        QuestionDataManager.sharedInstance.loadQuestion()
+
+        // 遷移先画面の呼び出し
+        guard let nextViewController = segue.destination as? QuestionViewController else {
+            // 取得できずに終了
+            return
+        }
+
+        // 問題文の取り出し
+        guard let questionData = QuestionDataManager.sharedInstance.nextQuestion() else {
+            // 取得できずに終了
+            return
+        }
+
+        // 問題文のセット
+        nextViewController.questionData = questionData
+
+        // タイトルに戻ってくるとき呼び出される処理
+        @IBAction func goToTitle(_ segue: UIStoryboardSegue) {
+
+        }
+    }
+}
